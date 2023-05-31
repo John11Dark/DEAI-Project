c630eb7f5a76b126ca10a78e71de6f3d6291259c
John Muller <Johnmuller2000@outlook.com>
2023-05-31 03:15:53 +0200

    add : fix paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  paths: {
+  define: {
     "../../types/*": ["./src/types/*"],
-    "./dashboard/9*": ["./src/screen/dashboard/*"],
+    "./dashboard/*": ["./src/screen/dashboard/*"],
   },
 });
